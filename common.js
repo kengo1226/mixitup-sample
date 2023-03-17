b7416b2dae80f3f39fff07ef434cfaff4327cd4d
岩本 <k_iwamoto@stride-web.co.jp>
2023-03-17 19:11:23 +0900

    コミットします

---
+++ common.js
@@ -0,0 +1,10 @@
+// var mixer = mixitup('.container');
+// var mixer = mixitup(containerEl);
+var mixer = mixitup('.container', {
+    selectors: {
+        target: '.mix'
+    },
+    animation: {
+        duration: 350
+    }
+});
